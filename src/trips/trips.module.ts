08d3f69e23f0619a66e6bcf33f6b6bc5b44d5107
Franco Giuliano <francosebastiangiuliano@gmail.com>
2026-01-10 10:10:09 -0300

    FEAT: Add trip delete functionality

--- src/trips/trips.module.ts
+++ src/trips/trips.module.ts
@@ -4,10 +4,19 @@ import { TripsService } from './trips.service';
 import { TripsController } from './trips.controller';
 import { Trip, TripSchema } from './trip.schema';
 import { ParticipantsModule } from '../participants/participants.module';
+import { Budget, BudgetSchema } from '../budgets/budget.schema';
+import {
+  Invitation,
+  InvitationSchema,
+} from '../participants/schemas/invitation.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Trip.name, schema: TripSchema }]),
+    MongooseModule.forFeature([
+      { name: Trip.name, schema: TripSchema },
+      { name: Budget.name, schema: BudgetSchema },
+      { name: Invitation.name, schema: InvitationSchema },
+    ]),
     forwardRef(() => ParticipantsModule),
   ],
   controllers: [TripsController],
